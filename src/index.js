cd8f42dc45f94dd03353d162040dab925be196cf
LightBlueGamer <timjonsson03@hotmail.com>
2022-12-16 19:09:35 +0100

    Final touch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { getURL } = require('./modules/getUrl');
 const { updateModpack } = require('./modules/updateModpack');
 const { hasUpdated } = require('./modules/checkForUpdate');
-const modpacks = require('./modpacks.json')
+const modpacks = require('../modpacks.json')
 
 async function run() {
     for(const {folder, id, sId} of modpacks) {
--- src/modules/updateModpack.js
+++ src/modules/updateModpack.js
@@ -1,8 +1,7 @@
 const { exec } = require('child_process');
-const { readdirSync } = require('fs');
+const { readFileSync } = require('fs');
 const JSZip = require("jszip");
 const zip = new JSZip();
-const { readFile } = require('fs');
 
 async function updateModpack(url, serverFolder, serverId) {
     exec(`cd "${serverFolder}" && curl "https://pan.litecraft.org/api/client/servers/${serverId}/power" \
@@ -15,32 +14,29 @@ async function updateModpack(url, serverFolder, serverId) {
         if (err) {
             console.error(err)
         } else {
-            exec(`wget "${url}"`, (err, stdout, stderr) => {
+            exec(`wget "${url}"`, async (err, stdout, stderr) => {
                 if (err) {
                     console.error(err)
                 } else {
                     const urlArr = url.split("/");
                     const zipFile = urlArr[urlArr.length-1];
                     let toDir = "";
-                    readFile(zipFile, (err, data) => {
-                        zip.loadAsync(data)
-                            .then((zip) => {
-                                const files = Object.keys(zip.files);
-                                const mapped = files.map(file => file.split('/')[0]);
-                                if([...new Set(mapped)].length > 1) toDir = `-d ${serverFolder}/${zipFile.replace('.zip', '')}`;
-                            })
-                    });
+                    let toRemove;
+                    const data = await readFileSync(zipFile);
+                    const zipped = await zip.loadAsync(data)
+                    const files = Object.keys(zipped.files);
+                    const mapped = files.map(file => file.split('/')[0]);
+                    toRemove = [...new Set(mapped)];
+                    if(toRemove.length > 1) toDir = `-d ${serverFolder}/${zipFile.replace('.zip', '')}`;
+                    console.log(`1: ${toRemove}`);
+                    console.log(`2: ${toDir}`);
+                    const command = `unzip "${zipFile}" ${toDir}`;
+                    console.log(command);
                     exec(`unzip "${zipFile}" ${toDir}`, (err, stdout, stderr) => {
                         if (err) {
                             console.error(err)
                         } else {
                             const modpackFolder = zipFile.replace('.zip', '');
-                            const packFiles = readdirSync(`${serverFolder}/${modpackFolder}`);
-                            const mainFiles = readdirSync(serverFolder);
-                            const toRemove = [];
-                            for(const file of packFiles) {
-                                if(mainFiles.includes(file)) toRemove.push(file);
-                            };
                             exec(`rm -drf ${toRemove.join(" ")}`, (err, stdout, stderr) => {
                                 if (err) {
                                     console.error(err)
